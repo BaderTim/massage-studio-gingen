2e711ebca1f895e60fac40ecdd2c43e8764a4771
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2021-10-08 15:05:01 +0200

    added some images, about text and fixed scrolling issues

--- src/content/pages/MobilePage.js
+++ src/content/pages/MobilePage.js
@@ -31,25 +31,32 @@ export default class MobilePage extends React.Component {
                 <div className={"content"}>
                 <div className={"entry-container"}>
                     <h1 className={"display-6 rhythm bold-sm"}>
-                        Massage Gingen
+                        Massagestudio Spa L.R.
                     </h1>
                     <h2 className={"display-6 rythm"} style={{fontSize: "15px"}}>
-                        Massage - Therapie - Spa
+                        Massage - Wellness - Spa
                     </h2>
-                    <br/>
+
 
                     <div className={"intro"}>
                         <p className={"lead"}>
                             &#187;Luxus für Körper, Seele und Geist.&#171;
                         </p>
                     </div>
-                    <br/>
-                    <p className={"lead intro-text"}>
-                        Anhaltender Stress setzt Ihrem Körper stark zu.
-                        Vertrauen Sie unserer Expertin, die Ihnen hilft,
-                        Ihre Verspannungen mit verschiedenen Massagen zu lösen.
-                    </p>
+
+                    <img className={"intro-image"}
+                         src={"images/uebersicht_2.jfif"}/>
+                         <br/>
                 </div>
+                    <div className={"bottom-container"}>
+                        <a href="https://api.whatsapp.com/send?phone=4915775200174&text=Hallo,%20ich%20m%C3%B6chte%20gerne%20einen%20Termin%20f%C3%BCr%20eine%20Massage%20vereinbaren%20:)" target="_blank"
+                           className="list-group-item list-group-item-action list-group-item-success wa-termin" rel="noreferrer">
+                            <img src={"wa_icon.png"} width={"20px"} alt={"wa_icon"}/>
+                            {" "} Jetzt Termin vereinbaren
+                        </a>
+                        <small className={"note"}>Oder telefonisch unter <a href={"tel:+4915775200174"}>+49 15775200174</a></small>
+                        <br/>
+                    </div>
                 <div className={"accordion-container"}>
                     <hr style={{margin: "0px"}}/>
                     <Accordion title={"Unser Angebot"}>
@@ -79,25 +86,33 @@ export default class MobilePage extends React.Component {
                     <hr style={{margin: "0px"}}/>
 
                     <Accordion title={"Über Uns"}>
-                        <h1>Yeet</h1><br/>
-                        <p>ok test 123 hahahaha</p>
+                        <p className={"lead"} style={{fontSize: "16px"}}>
+                            <strong>
+                                Anhaltender Stress setzt Ihrem Körper stark zu.{" "}
+                            </strong>
+                            In unserem Massagestudio in Gingen an der Fils
+                            erfahren Sie die ein rundum Wohlfühlerlebnis,
+                            das sich von Wellness bishin zu vielerlei
+                            professionellen Massagen erstreckt. <br/>Wir bieten Ihnen
+                            ein sanftes Kissen für Ihre Seele und weiche Hände
+                            für Ihre Verspannungen.
+                        </p>
+                        <img className={"intro-image"}
+                             src={"images/larissa.jfif"}/>
+                        <blockquote className="blockquote" style={{marginTop: "5px"}}>
+                            <p>Larisa Rißling</p>
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                            Professionelle Masseurin aus
+                            <cite title="Source Title"> Gingen an der Fils</cite>
+                        </figcaption>
+
                     </Accordion>
                     <hr style={{margin: "0px"}}/>
                 </div>
-                <div className={"bottom-container"}>
-                    <br/>
-                    <a href="https://api.whatsapp.com/send?phone=4915775200174&text=Hallo,%20ich%20m%C3%B6chte%20gerne%20einen%20Termin%20f%C3%BCr%20eine%20Massage%20vereinbaren%20:)" target="_blank"
-                       className="list-group-item list-group-item-action list-group-item-success wa-termin" rel="noreferrer">
-                        <img src={"wa_icon.png"} width={"20px"} alt={"wa_icon"}/>
-                        {" "} Jetzt Termin vereinbaren
-                    </a>
-                    <br/>
-                    <small className={"note"}>Oder telefonisch unter <a href={"tel:+4915775200174"}>+49 15775200174</a></small>
-                    <br/>
-                </div>
                 </div>
                 <div style={{marginTop: "auto"}}>
-                <Footer/>
+                    <Footer/>
                 </div>
             </div>
         )
